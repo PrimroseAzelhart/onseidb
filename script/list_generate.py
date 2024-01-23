220baaee53afdf781fb71a817b78539dc0891202
Starst <shiyunchen126@126.com>
2024-01-23 11:08:57 +0800

    [Auto] Use absolute path, add log

--- script/list_generate.py
+++ script/list_generate.py
@@ -1,10 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Execute once a day to update data list
-
 import json
 import time
+import datetime
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -22,25 +21,28 @@ def main():
     db = client['onseidb']
     record = {}
 
+    with open('/opt/app/log.txt', 'a', encoding='utf-8') as log:
+        log.write(f'[{datetime.datetime.now()}] Update data list\n')
+
     with open('update_record.json', 'r', encoding='utf-8') as frecord:
         record = json.load(frecord)
 
     if db['update'].find_one(doc['cv'])['update']:
-        with open('cv.json', 'w', encoding='utf-8') as fcv:
+        with open('/opt/app/cv.json', 'w', encoding='utf-8') as fcv:
             results = list(db['cv'].find({}, {"_id": 0, "name": 1}))
             json.dump(results, fcv)
             db['update'].update_one(doc['cv'], {'$set': updated})
             record['cv'] = int(time.time())
 
     if db['update'].find_one({'doc': 'circle'})['update']:
-        with open('circle.json', 'w', encoding='utf-8') as fcircle:
+        with open('/opt/app/circle.json', 'w', encoding='utf-8') as fcircle:
             results = list(db['circle'].find({}, {"_id": 0, "name": 1}))
             json.dump(results, fcircle)
             db['update'].update_one(doc['circle'], {'$set': updated})
             record['circle'] = int(time.time())
 
     if db['update'].find_one({'doc': 'tag'})['update']:
-        with open('tag.json', 'w', encoding='utf-8') as ftag:
+        with open('/opt/app/tag.json', 'w', encoding='utf-8') as ftag:
             results = list(db['tag'].find({}, {"_id": 0, "value": 1}))
             json.dump(results, ftag)
             db['update'].update_one(doc['tag'], {'$set': updated})
