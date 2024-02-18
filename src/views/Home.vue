697492c4d2692aed87f3c7a7db056c18fca11fe4
Starst <shiyunchen126@126.com>
2024-02-18 09:59:51 +0800

    [Icon] Use font-awesome css instead of scss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="card">
         <h4>This is OnseiDB</h4>
-        <div>Internal users only</div>
+        <div>Internal users only currently</div>
         <div>Before use, click on Database on the menubar and update local database</div>
     </div>
 </template>
